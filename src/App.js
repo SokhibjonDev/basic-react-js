6e2f0a9182dc3f90f5ea0b8c03584b2d3e659316
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-08-26 11:40:11 +0500

    feat : func ( toggleHandler )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,88 @@
-import './App.css';
+import { Component } from 'react';
+import Car from './Car'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+    cars: [
+      {
+        name: 'Ford',
+        year: 2015
+      },
+      {
+        name: 'Ferrari',
+        year: 1999
+      },
+      {
+        name: 'Skyline',
+        year: 2002
+      },
+    ],
+    title: 'React Components',
+    showCars: false
+  }
 
-    </div>
-  );
+  ToggleCarsHandler = (e) => {
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+  // onChangeTitleInputHandler = (e) => {     NOMER 2
+  //   this.setState({
+  //     title: e.target.value
+  //   })
+  // }
+
+  // onChangeTitleButtonHandler = (name) => {     NOMER 1
+  //   this.setState({
+  //     title: name
+  //   })
+  // }
+  render() {
+    const AppStyle = {
+      textAlign: 'center',
+    }
+    const buttonCSS = {
+      padding: '10px 30px',
+      background: 'transparent',
+      color: '#fff',
+      border: '1px solid #fff',
+      textTransform: 'uppercase',
+      cursor: 'pointer',
+      fontSize: 20,
+      letterSpacing: 2,
+    }
+    // const inputCSS = {
+    //   padding: '7px 10px',
+    // }      NOMER 2
+    let div = null
+    if (this.state.showCars) {
+      div = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={Math.random()}
+            name={car.name}
+            year={car.year}
+            // onClick={
+            //   this.onChangeTitleButtonHandler.bind(this, car.name)     NOMER 1
+            // }
+          />
+        )
+      })
+    }
+
+    return (
+      <div div className="App" style={AppStyle} >
+        <h1 style={{ fontSize: 60, margin: 10 }}>{this.state.title}</h1>
+        {/* <input style={inputCSS} type="text" onChange={this.onChangeTitleInputHandler} /><br /><br />      NOMER 2  */  }
+        <button style={buttonCSS} onClick={this.ToggleCarsHandler}>Click</button>
+        <br />
+        <br />
+        {
+          div
+        }
+      </div >
+    );
+  }
 }
 
 export default App;
---
+++ src/Car.js
@@ -0,0 +1,31 @@
+function Car(props) {
+    const carStyle = {
+        boxShadow: '0 0 10px #fff',
+        fontSize: '50px',
+        display: 'inline-block',
+        padding: '15px ',
+        margin: '15px',
+        textAlign: 'center',
+    }
+    // const ChangeBtn = {
+    //     padding: '10px 30px',
+    //     background: 'transparent',
+    //     color: '#fff',
+    //     border: '1px solid #fff',
+    //     textTransform: 'uppercase',
+    //     cursor: 'pointer',
+    //     fontSize: 20,
+    //     letterSpacing: 2,
+    // }            NOMER 1
+    return (
+        <div className="Car" style={carStyle}>
+            <h1>{props.name}</h1>
+            <p>{props.year}</p>
+            <span>Id:<strong>{Math.floor(Math.random() * 4)}</strong></span>
+            <br />
+            {/* <button style={ChangeBtn} onClick={props.onClick}>Change Title</button>     NOMER 1*/}
+        </div>
+    )
+}
+
+export default Car
